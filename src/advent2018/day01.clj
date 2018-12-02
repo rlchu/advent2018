8333259769fac73f69717c6a5f40b1d81e2236c9
bombinthebeehive <ryan.chupick@gmail.com>
2018-12-01 20:06:52 -0800

    cleanup

--- src/advent2018/day01.clj
+++ src/advent2018/day01.clj
@@ -1,21 +1,17 @@
 (ns advent2018.day01
   (:require [advent2018.helpers :refer [parse-int]]))
 
-(def data
-  (-> "inputs/day01.input" slurp ))
-
 (defn- data-clean [data]
   (->> (clojure.string/split data #"\n\+" )
-       (map #(clojure.string/split % #"\n"))
+       (mapv #(clojure.string/split % #"\n"))
        flatten
-       (map parse-int)))
+       (mapv parse-int)))
 
 (defn- checkset-down-list [accset xs]
-  (when (seq xs)
-   (let [next-digit (first xs)]
+  (let [next-digit (first xs)]
     (if (accset next-digit)
       next-digit
-      (recur (conj accset next-digit) (rest xs)))) ))
+      (recur (conj accset next-digit) (rest xs)))))
 
 (defn solve-01 [data]
   (reduce + (data-clean data)))
@@ -27,10 +23,10 @@
  "an attempt to speed up solve-02 by concating the list of data 
   to itself instead of cycling one token at a time"
  (defn solve [data]
-  (let [frequency-match (->> data
+  (let [frequency-match  (->> data
                              (reductions + 0)
                              (checkset-down-list #{}))] 
     (if frequency-match 
         frequency-match
-        (recur (concat data data))))))
+        (recur (concat data data data data))))))
  
