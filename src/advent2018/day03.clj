6426112492a5692b8f150dbfcac3220446785fc9
rlchu <ryan.chupick@gmail.com>
2019-01-08 13:18:02 -0800

    day3 part 2

--- src/advent2018/day03.clj
+++ src/advent2018/day03.clj
@@ -1,44 +1,40 @@
 (ns advent2018.day03
   (:require [advent2018.helpers :refer [parse-int]]))
 
-
-(def input 
-  "#1 @ 1,3: 4x4
-  #2 @ 3,1: 4x4
+  (def sample-input "#1 @ 1,3: 4x4
+#2 @ 3,1: 4x4
 #3 @ 5,5: 2x2")
 
-(def dat (clojure.string/split-lines input))
+(def input
+  (-> "inputs/day03.input" slurp ))
 
-(defn parse-out-coord-info [dat]
-  (->> dat 
+(def dat (clojure.string/split-lines sample-input))
+
+(defn- parse-out-coord-info [dat]
+  (->> dat
        (re-seq #"\d+")
        (drop 1)
        (map parse-int)))
 
-(defn generate-grid-points [x y xl yl]
+(defn- generate-grid-points [x y xl yl]
   (for [a (range x (+ x xl))
         b (range y (+ y yl))]
-   [a b]))
+    [a b]))
 
-(as-> dat d
-  (map parse-out-coord-info d)
-  (map #(apply generate-grid-points %) d)
-  (apply concat d))
+(defn- get-overlap-frequencies [dat]
+  (frequencies
+   (->> dat
+        (map parse-out-coord-info)
+        (map #(apply generate-grid-points %))
+        (apply concat)))
 
+  (defn solve-01 [dat]
+    (->> dat
+         get-overlap-frequencies
+         (filter (fn [[k v]] (not= 1 v)))
+         count
+         )))
 
-(def b '([3 1] [3 2] [3 3] [3 4] [4 1] [4 2] [4 3] [4 4] [5 1] [5 2] [5 3] [5 4] [6 1] [6 2] [6 3] [6 4]))
-
-(def a '([1 3] [1 4] [1 5] [1 6] [2 3] [2 4] [2 5] [2 6] [3 3] [3 4] [3 5] [3 6] [4 3] [4 4] [4 5] [4 6]))
-
-(def c '([5 5] [5 6] [6 5] [6 6]))
-
-
-(def d (frequencies 
-       (concat a b c)))
-
-(count (filter (fn [[k v]] (not= 1 v)) d))
-
-
-
+(solve-01 dat)
 
 
